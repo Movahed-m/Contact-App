6ce090f982fbcfb1fa47587929379ac587eec0b9
Mohammad Movahed <mohammad03@yahoo.com>
2024-09-01 16:31:40 +0330

    adding styles of ContactList component

--- src/components/Main/ContactList.jsx
+++ src/components/Main/ContactList.jsx
@@ -1,7 +1,8 @@
 import { useState } from "react";
 import ContactListItem from "./ContactListItem";
+import styles from "../../styles/Main/ContactList.module.css";
 
-const ContactList = ({ contacts, setContacts, setSelectedContact}) => {
+const ContactList = ({ contacts, setContacts, setSelectedContact }) => {
   const [selecteAll, setSelecteAll] = useState(false);
   const [alert, setAlert] = useState("");
   const [isShow, setIsShow] = useState(false);
@@ -14,37 +15,54 @@ const ContactList = ({ contacts, setContacts, setSelectedContact}) => {
   const deleteHandler = () => {
     setContacts([]);
     setAlert("All Contacts delete successfully ✔");
+    setTimeout(() => {
+      setAlert("");
+    }, 2000);
     setIsShow(false);
   };
 
   return (
-    <div>
-      <div>
-        <button onClick={() => setSelecteAll(true)}>
+    <div className={styles["contact-list-container"]}>
+      <div className={styles["contact-list-options"]}>
+        <button
+          onClick={() => setSelecteAll(true)}
+          className={selecteAll ? styles["selecte-button"] : styles["button"]}
+        >
           {selecteAll ? "Selected" : "Selecte All"}
         </button>
-        <button onClick={() => setIsShow(true)}>
+        <button
+          onClick={() => setIsShow(true)}
+          className={selecteAll ? styles["delete-button"] : styles["hide"]}
+        >
           {selecteAll && "Delete All"}
         </button>
         {isShow && (
-          <div>
+          <div className={styles["modal-container"]}>
             <p>Are you want to delete all contacts?</p>
-            <button onClick={deleteHandler}>Yes</button>
-            <button onClick={exitHandler}>No</button>
+            <div>
+              <button onClick={deleteHandler}>Yes</button>
+              <button onClick={exitHandler}>No</button>
+            </div>
           </div>
         )}
-        <p>{alert && alert}</p>
+        <p className={alert ? styles["delete-message"] : styles["hide"]}>
+          {alert && alert}
+        </p>
       </div>
       <ul>
         {contacts &&
           contacts.map((item) => (
             <div key={item.id}>
               <input type="checkbox" checked={selecteAll} />
-              <ContactListItem contact={item} selecteAll={selecteAll} setSelectedContact={setSelectedContact}/>
+              <ContactListItem
+                contact={item}
+                selecteAll={selecteAll}
+                setSelectedContact={setSelectedContact}
+              />
             </div>
           ))}
+        <p>{!contacts.length && "No Contact Added Yet"}</p>
       </ul>
-      <p>{!contacts.length && "No Contact Added Yet"}</p>
     </div>
   );
 };
