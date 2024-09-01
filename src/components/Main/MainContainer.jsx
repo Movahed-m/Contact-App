3f25f80dbfa9e77e80819c436cbd7ee6b47a4626
Mohammad Movahed <mohammad03@yahoo.com>
2024-09-01 15:06:57 +0330

    adding styles of MainContainer component

--- src/components/Main/MainContainer.jsx
+++ src/components/Main/MainContainer.jsx
@@ -1,3 +1,5 @@
+import styles from "../../styles/Main/MainContainer.module.css";
+
 const MainContainer = ({
   children,
   setSearchedContacts,
@@ -6,20 +8,26 @@ const MainContainer = ({
 }) => {
   const searchHandler = (event) => {
     const value = event.target.value;
-    setSearchedContacts(contacts.filter(item => item.email.includes([value]) || item.phone.includes([value])))
-    setIsSearch(true)
+    setSearchedContacts(
+      contacts.filter(
+        (item) => item.email.includes([value]) || item.phone.includes([value])
+      )
+    );
+    setIsSearch(true);
   };
 
   return (
-    <div>
-      <h1>
-        <a href="../../index.html">Contact App</a>
-      </h1>
-      <input
-        type="search"
-        placeholder="🔍 Search bettween Contacts ..."
-        onChange={searchHandler}
-      />
+    <div className={styles["main"]}>
+      <div className={styles["header"]}>
+        <h1>
+          <a href="../../index.html">Contact App</a>
+        </h1>
+        <input
+          type="search"
+          placeholder="🔍 Search bettween Contacts ..."
+          onChange={searchHandler}
+        />
+      </div>
       {children}
     </div>
   );
