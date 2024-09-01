7de52685e9729ad0a1a463ba08117bedf0571455
Mohammad Movahed <mohammad03@yahoo.com>
2024-09-01 14:07:03 +0330

    move copy option from contactlist component to contact component

--- src/components/Main/Contact.jsx
+++ src/components/Main/Contact.jsx
@@ -1,35 +1,50 @@
 import { useState } from "react";
 
-const Contact = ({ selectedContact, setContacts , contacts}) => {
-  const [isShow, setIsShow] = useState(false)
-  const [isContactShow, setIsContactShow] = useState(true)
+const Contact = ({ selectedContact, setContacts, contacts }) => {
+  const [isShow, setIsShow] = useState(false);
+  const [isContactShow, setIsContactShow] = useState(true);
+  const [copyAlert, setCopyAlert] = useState("");
 
   const { name, email, phone, photo } = selectedContact;
-const deleteHandler = () => {
-    setContacts(contacts.filter(item => item !== selectedContact))
-    setIsShow(false)
-    setIsContactShow(false)
-}
+
+  const deleteHandler = () => {
+    setContacts(contacts.filter((item) => item !== selectedContact));
+    setIsShow(false);
+    setIsContactShow(false);
+  };
+
+  const copyHandler = (event) => {
+    window.navigator.clipboard.writeText(event.target.innerText);
+    setCopyAlert("Copy into Clipboard ✔");
+    setTimeout(() => {
+      setCopyAlert(false);
+    }, 2000);
+  };
 
   return (
     <>
-    {isContactShow && <div>
-      <img src={photo} style={{width: "50px"}}/>
-      <h3>{name}</h3>
-      <p>Email: {email}</p>
-      <p>Phone: {phone}</p>
-      <div>
-        <button onClick={() => setIsShow(true)}>Delete Contact</button>
-        <button>Edit</button>
-      </div>
-    </div>}
-    {isShow && 
-    <div>
-        <p>Are You want to delete the Contact?</p>
-        <button onClick={deleteHandler}>Yes</button>
-        <button onClick={()=> setIsShow(false)}>No</button>
-    </div>
-    }
+      {isContactShow && (
+        <div>
+          <img src={photo} style={{ width: "50px" }} />
+          <h3>{name}</h3>
+          <p>Email: </p>
+          <span onClick={copyHandler}>{email}</span>
+          <p>Phoen Number: </p>
+          <span onClick={copyHandler}>{phone}</span>
+          <p>{copyAlert}</p>
+          <div>
+            <button onClick={() => setIsShow(true)}>Delete Contact</button>
+            <button>Edit</button>
+          </div>
+        </div>
+      )}
+      {isShow && (
+        <div>
+          <p>Are You want to delete the Contact?</p>
+          <button onClick={deleteHandler}>Yes</button>
+          <button onClick={() => setIsShow(false)}>No</button>
+        </div>
+      )}
     </>
   );
 };
--- src/components/Main/ContactListItem.jsx
+++ src/components/Main/ContactListItem.jsx
@@ -1,31 +1,16 @@
-import { useState } from "react";
-
 const ContactListItem = ({ contact, setSelectedContact }) => {
-  const [copyText, setCopyText] = useState("");
-  const [copyAlert, setCopyAlert] = useState("");
-
-  const copyHandler = (event) => {
-    setCopyText(event.target.innerText);
-    window.navigator.clipboard.writeText(copyText);
-    setCopyAlert("Copy into Clipboard ✔");
-    setTimeout(() => {
-      setCopyAlert(false);
-    }, 2000);
-  };
-
   return (
     <>
-      <li onClick={() => (setSelectedContact(contact))}>
+      <li onClick={() => setSelectedContact(contact)}>
         <img
           src={contact.photo}
           alt="profile photo"
           style={{ width: "50px" }}
         />
         <span>{contact.name}</span>
-        <span onClick={copyHandler}>{contact.email}</span>
-        <span onClick={copyHandler}>{contact.phone}</span>
+        <span>{contact.email}</span>
+        <span>{contact.phone}</span>
       </li>
-      <p>{copyAlert}</p>
     </>
   );
 };
